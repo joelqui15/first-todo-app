de81465e17d8e3082ca1b54de0ac0cd30d3fc4f4
joel <joelqui15@hotmail.com>
2026-02-22 21:54:09 -0900

    begin form validation

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-//header elements
+//header/filter elements
 const addTaskBtn = document.querySelector(".header__add-btn");
 const filterBtns = document.querySelectorAll(".task__filter-btn");
 
 // task elements
 const taskTemplate = document.querySelector("#task__template");
-const clonedTemplate = taskTemplate.content.cloneNode(true);
-const task = clonedTemplate.querySelector(".task");
-const taskTitle = task.querySelector(".task__title");
-const taskDescription = task.querySelector(".task__description");
+const taskList = document.querySelector(".tasks");
 
 //modal elements
 const modal = document.querySelector(".modal");
-const form = document.querySelector(".modal__container");
+const form1 = document.forms.form1;
+const formCloseBtn = form1.elements.closeBtn;
+
+// define state (data)
+
+let tasks = [];
+let currentFilter = "all";
+
+//open & close modal
+function openModal() {
+  modal.classList.remove("modal_hidden");
+}
+
+function closeModal() {
+  modal.classList.add("modal_hidden");
+}
+
+// validation functions
+
+function showError(input, messageElement) {}
+
+console.log(formCloseBtn);
